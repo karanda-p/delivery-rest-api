0160b5c779d468f9474fe8844671db1def0de1b7
Pavel Karanda <77533722+karanda-p@users.noreply.github.com>
2021-04-08 12:12:29 +0300

    Update

--- src/main/java/com/itfb/fooddeliveryservice/controller/CartController.java
+++ src/main/java/com/itfb/fooddeliveryservice/controller/CartController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class CartController {
 
--- src/main/java/com/itfb/fooddeliveryservice/controller/OrderController.java
+++ src/main/java/com/itfb/fooddeliveryservice/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class OrderController {
 
